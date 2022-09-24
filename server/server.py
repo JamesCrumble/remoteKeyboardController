87045b27e1e55528e5400ad4b5218fb8d8014534
Artem Traschinsky <68563296+JamesCrumble@users.noreply.github.com>
2022-09-24 22:35:02 +0300

    feat: set server port as setting value

--- server/server.py
+++ server/server.py
@@ -1,4 +1,8 @@
 import os
+
+SERVER_PORT: int = 7575
+assert 1025 <= SERVER_PORT <= 60000
+
 os.system('pip install json-server.py')
 os.system('pip install requests')
 os.system("json-server settings.json -b 7575")
